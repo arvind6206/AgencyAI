a551d594fce99c5d12a8b86c1777e7c9d14bd0d7
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-09-23 14:53:09 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
+  const [theme, setTheme] = useState('light')
   return (
-    <div>
-      Hii
+    <div className='dark:bg-black relative'>
+      <Navbar theme={theme} setTheme={setTheme}/>
     </div>
   )
 }
