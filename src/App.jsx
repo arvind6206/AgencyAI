15f9ff795530eb156a3170af307f8eab764a6198
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-09-25 11:00:25 +0530

    Hero comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   const [theme, setTheme] = useState(
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="dark:bg-black relative">
       <Navbar theme={theme} setTheme={setTheme} />
+      <Hero/>
     </div>
   );
 }
