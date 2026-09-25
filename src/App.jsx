5697bd9b774ef2a58da35b2f30aee3a3170e9c6d
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-09-25 11:35:39 +0530

    Services added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import TrustedBy from "./components/TrustedBy";
+import Services from "./components/Services";
 
 function App() {
   const [theme, setTheme] = useState(
@@ -12,6 +13,7 @@ function App() {
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero/>
       <TrustedBy/>
+      <Services/>
     </div>
   );
 }
